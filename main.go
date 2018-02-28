fd756d680f5f2f1f41e1d357bf06fa62e881846d
Jue <laporte.julie@gmail.com>
2018-02-28 18:13:55 -0500

    added infinite loop :)

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 )
 
 func main() {
-	os.Setenv("AWS_ACCESS_KEY_ID", "INSERT ACCESS KEY IS HERE")
-	os.Setenv("AWS_SECRET_ACCESS_KEY", "INSERT ACCESS KEY HERE")
+	os.Setenv("AWS_ACCESS_KEY_ID", "AKIAIWXVSIDZT7YVCRAA")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "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")
 
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
@@ -20,20 +20,37 @@ func main() {
 	client := sqs.New(sess)
 
 	queueUrl := "https://sqs.us-east-1.amazonaws.com/127686158421/Test.fifo"
+	ListenToQueue(client, queueUrl)
+}
 
-	result, err := client.ReceiveMessage(&sqs.ReceiveMessageInput{
-		QueueUrl:            aws.String(queueUrl),
-		MaxNumberOfMessages: aws.Int64(1),
-		MessageAttributeNames: aws.StringSlice([]string{
-			"All",
-		}),
-		WaitTimeSeconds: aws.Int64(5),
-	})
+func ListenToQueue(client *sqs.SQS, url string) error {
+	for {
+		log.Println("Try to pull message...")
+		result, err := client.ReceiveMessage(&sqs.ReceiveMessageInput{
+			QueueUrl:            aws.String(url),
+			MaxNumberOfMessages: aws.Int64(10),
+			MessageAttributeNames: aws.StringSlice([]string{
+				"All",
+			}),
+			WaitTimeSeconds: aws.Int64(5),
+		})
 
-	if err != nil {
-		log.Println(err.Error())
-		return
+		if err != nil {
+			log.Println(err.Error())
+			return err
+		}
+
+		log.Println("YAAAA", result.Messages)
+		for _, message := range result.Messages {
+			_, err := client.DeleteMessage(&sqs.DeleteMessageInput{
+				QueueUrl:      &url,
+				ReceiptHandle: message.ReceiptHandle,
+			})
+
+			if err != nil {
+				log.Println("Delete error", err)
+			}
+		}
 	}
-
-	log.Println("YAAAA", result.Messages)
+	return nil
 }
